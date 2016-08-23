378fcc26269979229c68140844c35317d70c5184
yingchenpeng <ychp@terminus.io>
2016-08-23 22:29:55 +0800

    update getIp method

--- common/src/main/java/com/ychp/coding/common/util/RequestUtil.java
+++ common/src/main/java/com/ychp/coding/common/util/RequestUtil.java
@@ -10,7 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 public class RequestUtil {
 
     public static String getIp(HttpServletRequest request){
-        return request.getRemoteAddr();
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 
     public static String getHost(HttpServletRequest request){
