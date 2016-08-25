970bace10275a2a9e7586a1728fe6ae9601aec19
yingchenpeng <ychp@terminus.io>
2016-08-25 21:12:43 +0800

    add getIpAddress

--- common/src/main/java/com/ychp/coding/common/util/RequestUtil.java
+++ common/src/main/java/com/ychp/coding/common/util/RequestUtil.java
@@ -1,6 +1,11 @@
 package com.ychp.coding.common.util;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Desc:
@@ -26,4 +31,43 @@ public class RequestUtil {
     public static String getHost(HttpServletRequest request){
         return request.getRemoteHost();
     }
+
+    private static final String httpUrl = "http://apis.baidu.com/apistore/iplookupservice/iplookup";
+
+    /**
+     * @param httpUrl
+     *            :请求接口
+     * @param httpArg
+     *            :参数
+     * @return 返回结果
+     */
+    public static String getIpAddress(String httpUrl, String httpArg, String apiKey) {
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        httpUrl = httpUrl + "?" + httpArg;
+
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            // 填入apikey到HTTP header
+            connection.setRequestProperty("apikey",  apiKey);
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
